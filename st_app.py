4f06cef2d65b59f377568c0eba8aaca0d32fe41d
hiroset0391 <takashi.hirose@bosai.go.jp>
2023-07-11 18:00:08 +0900

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -101,8 +101,8 @@ for i in range(len(lons)):
         g_list.append(temp[1])
         b_list.append(temp[2])
         color.append([int(temp[0]), int(temp[1]), int(temp[2]), 150]) #'Alpha' は透過度を表す値で、0から255の範囲で指定します。0は完全に透明、255は完全に不透明を意味します。
-    # else:
-    #     color.append([0,0,0,0]) 
+    else:
+        color.append([0,0,0,0]) 
 
 st.write(len(list(polygon)), len(list(color)))
 Layers = list()
